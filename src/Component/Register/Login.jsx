6b06b50c0b7002f2bc28519d9e3d847409f6209c
Vivek <viveklearning2003@gmail.com>
2024-01-01 15:37:28 +0530

    vivek commit

--- src/Component/Register/Login.jsx
+++ src/Component/Register/Login.jsx
@@ -2,10 +2,40 @@ import React from 'react'
 import { useState } from "react";
 import "./Form.css";
 import vid from "./1.mp4";
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+
 const Login = () => {
   const [validated, setValidated] = useState(false);
+  const [email, setemail] = useState('');
+  const [pass, setpass] = useState('');
+
+  const navigate = useNavigate();
 
   const handleSubmit = (event) => {
+
+    const loginData = {
+      email: email,
+      pass: pass
+    }
+    axios.post(`http://localhost:5000/LogIn`, loginData)
+      .then((res) => {
+        if (res.data === "exists") {
+          alert("Login Successfull");
+          navigate('/');
+        }
+        else if (res.data === "notexists") {
+          alert("Invalid Password");
+        }
+        else {
+          alert("UserNotFound");
+        }
+      })
+      .catch((err) => {
+        alert(loginData.email);
+      })
+
     const form = event.currentTarget;
     if (form.checkValidity() === false) {
       event.preventDefault();
@@ -18,26 +48,26 @@ const Login = () => {
   return (
     <div className="wrapper">
       <video src={vid} alt="" autoPlay={"true"} loop muted />
-    
+
 
       <div className="cont">
         <form>
           <h1>Login</h1>
           <div className="input">
             <label>Email:</label>
-            <input id="inp3" type="email" placeholder="Email" required />
+            <input id="inp3" type="email" placeholder="Email" required value={email} onChange={(e) => setemail(e.target.value)} />
           </div>
 
           <div className="input">
             <label>Password:</label>
-            <input id="inp5" type="password" placeholder="Enter Password" required />
+            <input id="inp5" type="password" placeholder="Enter Password" required value={pass} onChange={(e) => setpass(e.target.value)} />
           </div>
 
-          <button type="submit">Login</button>
+          <button type="submit" onClick={handleSubmit}>Login</button>
 
           <div className="login-link">
             <p>
-              Don't have an account?<a href="#">Sign Up</a>
+              Don't have an account?<Link to='/Sign'>SignUp</Link>
             </p>
           </div>
         </form>
--- src/Component/Register/Register.jsx
+++ src/Component/Register/Register.jsx
@@ -3,12 +3,37 @@ import React from "react";
 import { useState } from "react";
 import "./Form.css";
 import vid from "./1.mp4";
-
+import axios from 'axios';
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {
   const [validated, setValidated] = useState(false);
-
+  const [fname, setfname] = useState('');
+  const [lname, setlname] = useState('');
+  const [email, setemail] = useState('');
+  const [mobile, setmobile] = useState(0);
+  const [pass, setpass] = useState('');
+  const navigate = useNavigate();
   const handleSubmit = (event) => {
+
+    const signData = {
+      fname:fname,
+      lname:lname,
+      email:email,
+      mobile:mobile,
+      pass:pass
+    }
+    
+    axios.post('http://localhost:5000/Register',signData)
+    .then(()=>{
+      navigate('/Login');
+      alert("Sign Successful");
+    })
+    .catch((err)=>{
+      console.log(`error found ERROR:${err}`);
+    })
+
     const form = event.currentTarget;
     if (form.checkValidity() === false) {
       event.preventDefault();
@@ -29,36 +54,36 @@ const Register = () => {
 
           <div className="input">
             <label>Firstname:</label>
-            <input id="inp1" type="text" placeholder="Enter Firstname" required  />
+            <input id="inp1" type="text" placeholder="Enter Firstname"  required value={fname} onChange={(e)=>setfname(e.target.value)} />
           </div>
 
           <div className="input">
             <label>Lastname:</label>
-            <input id="inp2" type="text" placeholder="Enter Lastname" required />
+            <input id="inp2" type="text" placeholder="Enter Lastname" required value={lname} onChange={(e)=>setlname(e.target.value)} />
           </div>
 
           <div className="input">
             <label>Email:</label>
-            <input id="inp3" type="email" placeholder="Email" required />
+            <input id="inp3" type="email" placeholder="Email" required value={email} onChange={(e)=>setemail(e.target.value)} />
           </div>
 
           <div className="input">
             <label>Mobile No:</label>
-            <input id="inp4" type="tele" placeholder="Enter Mobile No." required />
+            <input id="inp4" type="tele" placeholder="Enter Mobile No." required value={mobile} onChange={(e)=>setmobile(e.target.value)} />
           </div>
 
           <div className="input">
             <label>Password:</label>
-            <input id="inp5" type="password" placeholder="Enter Password" required />
+            <input id="inp5" type="password" placeholder="Enter Password" required value={pass} onChange={(e)=>setpass(e.target.value)} />
           </div>
 
-       <button type="submit">Sign Up</button>
+       <button type="submit" onClick={handleSubmit}>Sign Up</button>
 
    
 
           <div className="login-link">
             <p>
-              Already have an account?<a href="#">Login</a>
+              Already have an account?<Link to='/Login'>Login</Link>
             </p>
           </div>
         </form>
